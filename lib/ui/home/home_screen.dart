96742033c7562aee8d9c7dc656f0b0334163e58f
cartacode <cartacode@outlook.com>
2019-11-14 05:41:18 -0500

    initialize project

---
+++ lib/ui/home/home_screen.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_redux_starter/ui/app/app_drawer_vm.dart';
+
+class HomeScreen extends StatelessWidget {
+
+  static final String route = '/home';
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Home'),
+      ),
+      drawer: AppDrawerBuilder(),
+    );
+  }
+}
