429d120946e112609b92eda20126a586a4906a07
cartacode <cartacode@outlook.com>
2019-11-18 09:26:07 -0500

    implement draft design

--- ios/Flutter/flutter_export_environment.sh
+++ ios/Flutter/flutter_export_environment.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
 export "FLUTTER_ROOT=G:\flutter"
-export "FLUTTER_APPLICATION_PATH=G:\projects\Flutter\NetworkingFlutter"
+export "FLUTTER_APPLICATION_PATH=G:\projects\Flutter\deinort_app"
 export "FLUTTER_TARGET=lib\main.dart"
 export "FLUTTER_BUILD_DIR=build"
 export "SYMROOT=${SOURCE_ROOT}/../build\ios"
--- lib/widgets/newsList.dart
+++ lib/widgets/newsList.dart
@@ -9,7 +9,7 @@ import 'package:deinort_app/utils/constants.dart';
 class NewsListState extends State<NewsList> {
 
   List<NewsArticle> _newsArticles = List<NewsArticle>();
-  UserLocation _location = new UserLocation();
+  // var _location = new Location();
 
   @override
   void initState() {
@@ -17,12 +17,9 @@ class NewsListState extends State<NewsList> {
     _populateNewsArticles(); 
   }
 
-  Future<Map<String,dynamic>> _getLocation() {
+  Future<UserLocation> _getLocation() {
     return Webservice().load(UserLocation.info).then((location) {
-      return {
-        'address': location.address
-      };
-      
+      return location;
     });
   }
 
@@ -45,15 +42,68 @@ class NewsListState extends State<NewsList> {
       );
   }
 
+  // Widget newsListWidget = Stack(
+  //   children: <Widget>[
+  //     Positioned(
+  //       top: 0.0,
+  //       child: Image.asset(
+  //         'assets/google.jpg',
+  //         fit: BoxFit.fill,
+  //       ),
+  //     ),
+  //     Center(
+  //       child: ListView.builder(
+  //         itemCount: _newsArticles.length,
+  //         itemBuilder: _buildItemsForListView,
+  //       )
+  //     ),
+  //   ],
+  // );
+
   @override
   Widget build(BuildContext context) {
+    Size size = MediaQuery.of(context).size;
     return Scaffold(
         appBar: AppBar(
           title: Text('News'),
         ),
-        body: ListView.builder(
-          itemCount: _newsArticles.length,
-          itemBuilder: _buildItemsForListView,
+        body: Stack(
+          children: <Widget>[
+            Center(
+              child: new Image.asset(
+                'assets/google.jpg',
+                width: size.width,
+                height: size.height,
+                fit: BoxFit.fill,
+              ),
+            ),
+            Container(
+              height: 300,
+              margin: const EdgeInsets.only(top: 150.0),
+              padding: const EdgeInsets.only(top: 10.0),
+              color: Colors.blue,
+              child: Stack(children: <Widget>[
+                Text(
+                  'Street1, Hemmingun',
+                  style: TextStyle(fontWeight: FontWeight.bold,
+                  fontSize: 18,
+                  color: Colors.black),
+                ),
+                Container(
+                  margin: EdgeInsets.only(
+                    top: 40,
+                    right: 10,
+                    bottom: 10,
+                    left: 10),
+                  color: Color(0xFFFFFFFF),
+                  child: ListView.builder(
+                    itemCount: _newsArticles.length,
+                    itemBuilder: _buildItemsForListView,
+                  ),
+                )
+              ],),
+            )
+          ],
         )
       );
   }
