653a8749ed549b812d3afad30c04e7a805ce12eb
cartacode <cartacode@outlook.com>
2019-11-21 13:48:28 -0500

    add search functionality

--- lib/models/location.dart
+++ lib/models/location.dart
@@ -20,17 +20,32 @@ class UserLocation {
     return UserLocation(
       latitude: json['latitude'], 
       longitude: json['longitude'], 
-      address: json['staddress'] ?? '',
-      zipcode: json['zipcode'] ?? '',
-      city: json['city'] ?? '',
-      state: json['state'] ?? '',
-      country: json['country'] ?? '',
+      address: json['staddress'],
+      zipcode: json['zipcode'],
+      city: json['city'],
+      state: json['state'],
+      country: json['country'],
     );
+  }
+
+  factory UserLocation.fromReverseJson(Map<String,dynamic> json) {
+    if (json.containsKey('alt')) {
+      return UserLocation(
+        latitude: json['alt']['loc']['latt'], 
+        longitude: json['alt']['loc']['longt'], 
+        address: json['alt']['loc']['staddress'],
+        zipcode: json['alt']['loc']['postal'],
+        city: json['alt']['loc']['city'],
+        state: json['alt']['loc']['state'],
+        country: json['alt']['loc']['country'],
+      );
+    } else {
+      throw Exception('Failed to load data!');
+    }
 
   }
 
   static Resource<UserLocation> get info {
-    
     return Resource(
       url: Constants.GEOCODE_DEMO_URL,
       parse: (response) {
@@ -38,7 +53,16 @@ class UserLocation {
         return UserLocation.fromJson(result);
       }
     );
+  }
 
+  static Resource<UserLocation> get searchByCity {
+    return Resource(
+      url: Constants.GEOCODE_DEMO_URL,
+      parse: (response) {
+        final result = json.decode(response.body); 
+        return UserLocation.fromReverseJson(result);
+      }
+    );
   }
 
 }
--- lib/utils/times.dart
+++ lib/utils/times.dart
@@ -9,12 +9,12 @@ String readTimestamp(String created) {
   var time = '';
 
   if (diff.inSeconds <= 0 || diff.inSeconds > 0 && diff.inMinutes == 0 || diff.inMinutes > 0 && diff.inHours == 0 || diff.inHours > 0 && diff.inDays == 0) {
-    time = format.format(date);
+    time = 'heute - ' + format.format(date);
   } else if (diff.inDays > 0 && diff.inDays < 7) {
     if (diff.inDays == 1) {
-      time = diff.inDays.toString() + ' DAY AGO';
+      time = 'gestern - ' + format.format(date);
     } else {
-      time = diff.inDays.toString() + ' DAYS AGO';
+      time = diff.inDays.toString() + ' DAYS AGO - ' + format.format(date);
     }
   } else {
     if (diff.inDays == 7) {
